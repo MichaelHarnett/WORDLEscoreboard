245f0d8f6f9988718bc88c8a2422a8f3453df58f
Michael Harnett <62779855+MichaelHarnett@users.noreply.github.com>
2022-04-03 22:33:33 -0400

    adding average guess scores

--- app.py
+++ app.py
@@ -25,20 +25,52 @@ daddf = pd.read_csv('tables/daddf.csv')
 dadwindf = pd.read_csv('tables/dadwindf.csv')
 
 
+
+########################
+##### calculations #####
+########################
+
+### win percents
 michael_winpercent = (len(michaelwindf)/len(michaeldf)*100)
 dad_winpercent = (len(dadwindf)/len(daddf)*100)
 danielle_winpercent = (len(daniellewindf)/len(danielledf)*100)
 chris_winpercent = (len(chriswindf)/len(chrisdf)*100)
 
+### average guesses
+michaelwindf.score = michaelwindf.score.astype(int)
+michael_avg = michaelwindf.score.sum()/len(michaelwindf)
 
+daniellewindf.score = daniellewindf.score.astype(int)
+danielle_avg = daniellewindf.score.sum()/len(daniellewindf)
+
+chriswindf.score = chriswindf.score.astype(int)
+chris_avg = chriswindf.score.sum()/len(chriswindf)
+
+dadwindf.score = dadwindf.score.astype(int)
+dad_avg = dadwindf.score.sum()/len(dadwindf)
+
+
+
+
+##################
+##### prints #####
+##################
+### michael's info
 st.header('Michael')
 st.write('Win percent:' , michael_winpercent)
+st.write('Average guess score:', michael_avg)
 
+### danielle's info
 st.header('Danielle')
 st.write('Win percent:', danielle_winpercent)
+st.write('Average guess score:', danielle_avg)
 
+###Chris' info
 st.header('Chris')
 st.write('Win percent:', chris_winpercent)
+st.write('Average guess score:', chris_avg)
 
+### Dad's info
 st.header('Dad')
 st.write('Win percent:', dad_winpercent)
+st.write('Average guess score:', dad_avg)
