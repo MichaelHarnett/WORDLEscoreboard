0625c95975a97d4bfedfe9cedd88e0381020bc40
Michael Harnett <62779855+MichaelHarnett@users.noreply.github.com>
2022-04-17 03:16:46 -0400

    ok trying one more thing for the graph

--- app.py
+++ app.py
@@ -184,10 +184,11 @@ with st.expander('Missing Game Numbers'):
     st.write(michael_missing)
        
     
-# michael_guesschart = plt.gca()
-# michael_guesschart.invert_yaxis()
+# michael_guesschart = plt.gca().invert_yaxis()
 
 # michael_guesschart.barh(x_range, michael_guesscount)
+st.bar_chart(x_range, michael_guesscount)    
+
     
 
 ### danielle's info
