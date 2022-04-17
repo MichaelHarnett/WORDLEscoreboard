380ef83190279f79255b0aa02d2820425f44bf7f
Michael Harnett <62779855+MichaelHarnett@users.noreply.github.com>
2022-04-17 03:09:40 -0400

    testing graphs

--- app.py
+++ app.py
@@ -182,6 +182,12 @@ st.write('Win Percent:' , michael_winpercent)
 st.write('Average Guess Score:', michael_avg)
 with st.expander('Missing Game Numbers'):
     st.write(michael_missing)
+       
+    
+michael_guesschart = plt.gca()
+michael_guesschart.invert_yaxis()
+
+michael_guesschart.barh(x_range, michael_guesscount)
     
 
 ### danielle's info
