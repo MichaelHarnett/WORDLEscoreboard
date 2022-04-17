90b70a40ca64d7e8b1df9f1f9c385663c92870c8
Michael Harnett <62779855+MichaelHarnett@users.noreply.github.com>
2022-04-17 13:29:44 -0400

    adding matplotlib to imports

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objs as go
+import matplotlib.pyplot as plt
 from plotly import tools
 from plotly.subplots import make_subplots
 
