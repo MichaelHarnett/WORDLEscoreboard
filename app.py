96d9d29bd794b0782fa86913eefbeb8cc31b5b7c
Michael Harnett <62779855+MichaelHarnett@users.noreply.github.com>
2022-04-17 23:48:06 -0400

    meow

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ michael_guesschart = plt.figure()
 michael_guesschart = plt.gca()
 michael_guesschart.invert_yaxis()
 michael_guesschart = michael_guesschart.barh(x_range, michael_guesscount)
-st.show(michael_guesschart)
+st.plt(michael_guesschart)
 
 
 
