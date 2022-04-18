0b9362d32cfe42a2f46e645524a362eeb7469490
Michael Harnett <62779855+MichaelHarnett@users.noreply.github.com>
2022-04-17 23:54:13 -0400

    lskdjf

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ michael_guesschart = plt.figure()
 michael_guesschart = plt.gca()
 michael_guesschart.invert_yaxis()
 michael_guesschart = michael_guesschart.barh(x_range, michael_guesscount)
-micheal_chartdata = (x_range, michael_guesscount)
+michael_chartdata = (x_range, michael_guesscount)
 st.bar_chart(michael_chartdata)
 
 
