2d621c57fe00e8a22c0558ddd1ff4fd397d5ff8f
Michael Harnett <62779855+MichaelHarnett@users.noreply.github.com>
2022-04-23 23:21:24 -0400

    fixing file paths

--- app/pages/family.py
+++ app/pages/family.py
@@ -15,14 +15,14 @@ def app():
     ### read ins ###
     ################
     #michael
-    michaeldf = pd.read_csv('../tables/michaeldf.csv')
-    michaelwindf = pd.read_csv('../tables/michaelwindf.csv')
+    michaeldf = pd.read_csv('./tables/michaeldf.csv')
+    michaelwindf = pd.read_csv('./tables/michaelwindf.csv')
     #danielle
     danielledf = pd.read_csv('./tables/danielledf.csv')
     daniellewindf = pd.read_csv('./tables/daniellewindf.csv')
     #chris
-    chrisdf = pd.read_csv('tables/chrisdf.csv')
-    chriswindf = pd.read_csv('tables/chriswindf.csv')
+    chrisdf = pd.read_csv('./tables/chrisdf.csv')
+    chriswindf = pd.read_csv('./tables/chriswindf.csv')
     #dad
     daddf = pd.read_csv('tables/daddf.csv')
     dadwindf = pd.read_csv('tables/dadwindf.csv')
